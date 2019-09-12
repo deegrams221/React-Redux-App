6e8ca7aa02f68257d442c3baf03c7dc6d88dbaa9
Diana Grams <deegrams221@gmail.com>
2019-09-12 14:00:10 -0600

    set up initialState/ reducers in reducers.index.js, built out components/import connect/set up mSTP

--- new-app/src/components/dog.js
+++ new-app/src/components/dog.js
@@ -1,3 +1,33 @@
 // Step 5 - Build out components
 // Step 6 - import connect and set up mSTP
 
+import React, {useEffect} from 'react';
+import {connect} from 'react-redux';
+import {getDog} from '../store/actions';
+
+const Dogs = ({getDog, dog, isFetching}) => {
+  useEffect(() => {
+    getDog();
+  }, [getDog])
+
+  if(isFetching) {
+    return <h2>Fetching A Random Dog!</h2>
+  }
+
+  return (
+    <>
+      <h2>{dog}</h2>
+      <button onClick={getDog}>See Another Dog</button>
+    </>
+  )
+};
+
+const mapStateToProps = state => {
+  return {
+    insult: state.insults,
+    isFetching: state.isFetching,
+    error: state.error
+  };
+};
+
+export default connect(mapStateToProps,{getDog})(Dogs);
--- new-app/src/store/reducers/index.js
+++ new-app/src/store/reducers/index.js
@@ -2,3 +2,16 @@
 
 // Step 9 - set up Reducer logic for actions
 
+const initialState = {
+  insults: [],
+  isFetching: false,
+  error: ''
+};
+
+export const reducer = (state = initialState, action) => {
+  switch(action.type) {
+
+    default:
+      return state;
+  }
+}
