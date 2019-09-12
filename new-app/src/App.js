1f1424cdb6902ae268a4dc273680daefec7ca067
Diana Grams <deegrams221@gmail.com>
2019-09-12 14:04:43 -0600

    set up action creators/types, import axios/set up dispatch in actions.index.js, and import Dogs from components.dog.js into App.js

--- new-app/src/App.js
+++ new-app/src/App.js
@@ -2,12 +2,16 @@
 
 import React from 'react';
 
+// import Dogs from components
+import Dogs from './components/dog';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App-header">
       <h1>Cute Dog Image Generator</h1>
+      <Dogs />
     </div>
   );
 }
--- new-app/src/store/actions/index.js
+++ new-app/src/store/actions/index.js
@@ -2,3 +2,30 @@
 // Step 8 - import Axios and set up dispatch
 //        axios.get(`https://dog.ceo/api/breeds/image/random`)
 
+import axios from 'axios';
+
+// action types
+export const FETCH_DOG_START = 'FETCH_DOG_START';
+export const FETCH_DOG_SUCCESS = 'FETCH_DOG_SUCCESS';
+export const FETCH_DOG_FAIL = 'FETCH_DOG_FAIL';
+
+// action creators
+export const getDog = () => {
+  return dispatch => {
+    // dispatch and axios request
+    dispatch({type: FETCH_DOG_START});
+    axios
+      .get(`https://dog.ceo/api/breeds/image/random`)
+      .then(res => {
+        // console.log("axios request: ", res.data)
+        dispatch({type: FETCH_DOG_SUCCESS, payload: res.data});
+      })
+      .catch(error => {
+        console.log("catch error: ", error.response.message);
+        dispatch({
+          type: FETCH_DOG_FAIL,
+          payload: error.response.message
+        });
+      });
+  };
+};
